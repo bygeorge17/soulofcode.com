eb12f3a2b47772e34348ba7030cdb40e68e76de9
Jorge L Rios <rios.a.jorge.luis@gmail.com>
2025-01-18 13:23:13 -0700

    some changes

--- js/main.js
+++ js/main.js
@@ -31,3 +31,26 @@ $(document).ready(function() {
     ]
   });
 });
+
+
+  (function() {
+    // Reemplaza 'TU_USER_ID' con tu user ID de EmailJS
+    emailjs.init("dECp_1Fm9NpcTx_aA")
+  })();
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  // El método sendForm envía los datos del formulario
+  // Reemplaza "TU_SERVICE_ID" y "TU_TEMPLATE_ID" con los datos de tu cuenta EmailJS
+  emailjs.sendForm("service_7x3fnwg", "template_w8n1krh", this)
+    .then(function(response) {
+      console.log("¡Correo enviado con éxito!", response.status, response.text);
+      alert("¡Tu mensaje ha sido enviado correctamente!");
+      // Opcional: aquí puedes limpiar el formulario
+      event.target.reset();
+    }, function(error) {
+      console.log("Hubo un error al enviar el correo", error);
+      alert("Lo sentimos, no se pudo enviar tu mensaje. Intenta más tarde.");
+    });
+});
